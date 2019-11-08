d92d45cde1307cf97cd134a56a0a6fa921205ae2
Miguel Vieira <54644862+TheEletricboy@users.noreply.github.com>
2019-11-08 10:49:26 +0000

    channel 4 source code stuff

---
+++ Plural Javascript Getting Started/script.js
@@ -0,0 +1,9 @@
+console.log('Welcome to BlackJack!')
+
+let deck =["Ace of Spades", "Two of Spades", "Three of Spades"]
+
+let playeCards = [deck[0], deck[2]]
+
+console.log("You were dealt:");
+console.log(playeCards[0], playeCards[1])
+
